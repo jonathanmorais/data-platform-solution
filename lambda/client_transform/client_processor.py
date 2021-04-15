132615ca59b058aa9c203664fc91a82cedae8f95
jonathanmorais <jonathan.lucena@zoom.com.br>
2021-04-15 11:52:55 -0300

    run processor

--- lambda/client_transform/client_processor.py
+++ lambda/client_transform/client_processor.py
@@ -1,41 +1,45 @@
 from __future__ import print_function
 import json
 import base64
-import dask as dd
+import csv
+import pandas as pd
+import numpy
 
 print('Loading function')
 
 
 def lambda_handler(event, context):
-    output = []
+    output = []    
+    succeeded_record_cnt = 0
+    failed_record_cnt = 0
 
     for record in event['records']:
-        payload = base64.b64decode(record['data'])
-        pay = json.loads(base64.b64decode(payload).decode('ascii'))
+        print(record['recordId'])
+        payload = base64.b64decode(record['data']).decode('utf-8')
+        payload = base64.b64decode(payload)
+        payload = json.loads(payload)
 
         cleaned = {
-            "id": pay['id'],
-            "name": pay['name'],
-            "abv": pay['abv'],
-            "ibu": pay['ibu'],
-            "target_fg": pay['target_fg'],
-            "target_og": pay['target_og'],
-            "ebc": pay['ebc'],
-            "srm": pay['srm'],
-            "ph": pay['ph']
+            "id": payload['id'],
+            "name": payload['name'],
+            "abv": payload['abv'],
+            "ibu": payload['ibu'],
+            "target_fg": payload['target_fg'],
+            "target_og": payload['target_og'],
+            "ebc": payload['ebc'],
+            "srm": payload['srm'],
+            "ph": payload['ph']
         }
 
-        print(cleaned)
-
-        df = dd.read_json(cleaned).to_csv("cleaned.csv", index = None)
-
+        json_csv = pd.json_normalize(cleaned)
+     
         output_record = {
             'recordId': record['recordId'],
             'result': 'Ok',
-            'data': base64.b64encode(payload)
+            'data': base64.b64encode(json_csv.to_csv().encode('utf-8')).decode('utf-8')
         }
+        
         output.append(output_record)
-
-    print('Successfully processed {} records.'.format(len(event['records'])))
-
-    return df
+    print('Processing completed.  Successful records {}, Failed records {}.'.format(succeeded_record_cnt, failed_record_cnt))
+    print(output)
+    return {'records': output}
