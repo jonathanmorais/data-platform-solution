3fa815b84a76615f666a434f5953e0cfde9d1914
jonathanmorais <jonathan.lucena@zoom.com.br>
2021-04-18 18:35:48 -0300

    serving model ml

--- app/api.py
+++ app/api.py
@@ -1,59 +1,33 @@
-
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-import pickle
+from flask_restful import Api, Resource, reqparse
+import joblib
 import numpy as np
-from model import NLPModel
 
-app = Flask(__name__)
-api = Api(app)
+APP = Flask(__name__)
+API = Api(APP)
 
-model = NLPModel()
+MODEL = joblib.load('docker/workspace/model_punkbeer.sav')
 
-clf_path = 'lib/models/classify_punkapi.pkl'
-with open(clf_path, 'rb') as f:
-    model.clf = pickle.load(f)
+class Predict(Resource):
 
-vec_path = 'lib/models/TFIDFVectorizer.pkl'
-with open(vec_path, 'rb') as f:
-    model.vectorizer = pickle.load(f)
+    @staticmethod
+    def post():
+        parser = reqparse.RequestParser()
+        parser.add_argument('petal_length')
+        parser.add_argument('petal_width')
+        parser.add_argument('sepal_length')
+        parser.add_argument('sepal_width')
 
-# argument parsing
-parser = reqparse.RequestParser()
-parser.add_argument('query')
+        args = parser.parse_args()  # creates dict
 
+        X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
 
-class PredictSentiment(Resource):
-    def get(self):
-        # use parser and find the user's query
-        args = parser.parse_args()
-        user_query = args['query']
+        out = {'Prediction': MODEL}
 
-        # vectorize the user's query and make a prediction
-        uq_vectorized = model.vectorizer_transform(np.array([user_query]))
-        prediction = model.predict(uq_vectorized)
-        pred_proba = model.predict_proba(uq_vectorized)
-
-        # Output either 'Negative' or 'Positive' along with the score
-        if prediction == 0:
-            pred_text = 'Negative'
-        else:
-            pred_text = 'Positive'
-            
-        # round the predict proba value and set to new variable
-        confidence = round(pred_proba[0], 3)
-
-        # create JSON object
-        output = {'prediction': pred_text, 'confidence': confidence}
-        
-        return output
+        return out, 200
 
 
-# Setup the Api resource routing here
-# Route the URL to the resource
-api.add_resource(PredictSentiment, '/')
-
-
+API.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True, port='1080')
